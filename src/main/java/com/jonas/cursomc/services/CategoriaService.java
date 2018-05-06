7ed81c790eea65b26712abb9f6e22f3dd84355c7
Jonas Karlos <jonaskarlos@gmail.com>
2018-05-06 19:03:21 -0300

    Criando repository

---
+++ src/main/java/com/jonas/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.jonas.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jonas.cursomc.domain.Categoria;
+import com.jonas.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+				
+	}
+
+}

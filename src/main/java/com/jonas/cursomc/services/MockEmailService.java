f3784ccc7f0c3bfe663875eb0524531fdcfe1e35
Jonas Karlos <jonaskarlos@gmail.com>
2018-07-17 16:41:00 -0300

    Email HTML

--- src/main/java/com/jonas/cursomc/services/MockEmailService.java
+++ src/main/java/com/jonas/cursomc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.jonas.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -14,5 +16,15 @@ public class MockEmailService extends AbstractEmailService {
 		log.info(msg.toString());
 		log.info("Email enviado");
 	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		log.info("Simulando envio de e-mail HTML");
+		log.info(msg.toString());
+		log.info("Email enviado");
+				
+	}
+	
+	
 
 }
--- src/main/java/com/jonas/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/jonas/cursomc/services/SmtpEmailService.java
@@ -1,10 +1,13 @@
 package com.jonas.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService {
 	
@@ -12,6 +15,9 @@ public class SmtpEmailService extends AbstractEmailService {
 	
 	@Autowired
 	private MailSender mailSender;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
@@ -19,6 +25,14 @@ public class SmtpEmailService extends AbstractEmailService {
 		mailSender.send(msg);
 		log.info("Email enviado");
 		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		log.info("Enviando e-mail");
+		javaMailSender.send(msg);
+		log.info("Email enviado");
+				
 	}
 
 }
